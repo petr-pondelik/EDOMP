3c2932ee9ff1d92a80cc8206f6aad9bdfa63074e
Petr Pondělík <pondepe1@fit.cvut.cz>
2019-07-31 16:53:09 +0200

    Package.json optimized.

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -4,7 +4,6 @@ const merge = require('webpack-merge');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpackConfig = require('./webpack.config');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const ROOT_DIR = path.resolve(__dirname, './../');
 const WWW_DIR = path.resolve(ROOT_DIR, 'www');
