eaaebbbacb1772ba9995dd61126892b82bf4f1b1
Petr Pondělík <pondepe1@fit.cvut.cz>
2019-12-07 20:57:31 +0100

    Done: TestFunctionality, Authenticator and Authorizazor tests.

---
+++ Tests/TeacherModule/Services/ConditionServiceIntegrationTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wiedzmin
+ * Date: 7.12.19
+ * Time: 17:15
+ */
+
+namespace App\Tests\TeacherModule\Services;
+
+
+class ConditionServiceIntegrationTest
+{
+
+}
